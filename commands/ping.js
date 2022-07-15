a71569a4ac1d3c166d8cfe4ec29033ed11e45eda
Christopher Johnson <40903935+xSonicWavex@users.noreply.github.com>
2022-07-14 21:44:49 -0500

    Made a mistake Fixed it

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'ping', 
     permissions: ["SEND_MESSAGES"],
     description: "This is a ping command!",
-    execute(messageCreate, args, cmd, client, Discord, profileData){
+    execute(Discord, client, messageCreate, args, cmd){
         messageCreate.channel.send('pong!');
         messageCreate.reply("Sid's a bitch for talking behind my back. ~Sonic");
     }
